dce2f820d374a4d5e83789ba9d6f403382748998
santiagosuarez32 <dlsantii86@gmail.com>
2024-09-27 13:07:40 -0300

    listo

---
+++ main.js
@@ -0,0 +1,7 @@
+const tequiero = ["Si", "si"]
+const ingresar = prompt ("Me queres?")
+if (tequiero.includes(ingresar)) {
+    alert (`Yo tambien te quiero hermosa`)
+} else {
+    alert(`Que mala sos`)
+}
